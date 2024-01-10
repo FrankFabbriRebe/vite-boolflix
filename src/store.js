46d47807d5a8ff1259e370ca63f9be5919e537d6
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-10 17:07:24 +0100

    add: apiURL and moviesArray

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+
+    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=46ea676c2ff270a694d3966bc0f52218&language=it-IT',
+    moviesArray: [],
+
+
+})
