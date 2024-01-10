f1e95f6962c5a2f7c119e64621c85ad42574580b
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-10 17:02:11 +0100

    add: content in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,36 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <div class="container">
 
-<style scoped></style>
+        <div class="card">
+            <!-- debug -->
+            <h1>immagine</h1>
+            <!-- <img src="" alt=""> -->
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.container {
+    width: 80%;
+    margin: 20px auto;
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+    justify-content: center;
+    /* debug */
+    height: 500px;
+    border: 2px solid black;
+}
+
+.card {
+    width: calc(100% / 3 - 10px);
+    /* debug */
+    background-color: blue;
+    height: 200px;
+
+}
+</style>
