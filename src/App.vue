5ee469c2e902f5b6f107f7d963b8eaf8ace195e5
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-10 17:59:37 +0100

    import: store export: components and store

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
-<script setup>
+<script>
 
 // import components
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
+// import store
+import { store } from './store';
+
+export default {
+
+  components: {
+    AppHeader,
+    AppMain,
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+
+}
+
+console.log(store.movieSearched);
+
 </script>
 
 <template>
   <AppHeader />
+  <!-- <AppHeader @search="searchMedia" /> -->
   <AppMain />
 </template>
 
