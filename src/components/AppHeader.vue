9ce53708ec772fec2f5a7a25e1f4ba328b329c41
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-10 16:40:22 +0100

    add: AppHeader component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,6 +7,28 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="logo">
+            <h1>BOOLFLIX</h1>
+        </div>
+        <div class="searc_bar">
+            <input type="text" placeholder="Cerca il film/serie tv">
+            <button>Search</button>
+        </div>
+    </header>
+</template>
 
-<style scoped></style>
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: black;
+    padding: 20px;
+}
+
+h1 {
+    color: red;
+}
+</style>
