2a8d71d2c4ad6e73817822851a14fe1dcaff031e
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-10 16:45:34 +0100

    import: AppMain component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 
 // import components
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
 </script>
 
 <template>
   <AppHeader />
+  <AppMain />
 </template>
 
 <style lang="scss">
