8d99ab0c4f3cb92d730de456126e67d0a193a8f2
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-11 10:02:26 +0100

    create: AppCard component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,33 @@
+<script>
+
+import { store } from '../store';
+
+export default {
+    name: 'AppCard',
+
+    data() {
+        return {
+            store,
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div v-for="(element, i) in store.moviesArray" :key="i" class="card">
+        <!-- <h1>immagine</h1> -->
+        <img :src="element.poster_path" alt="">
+        <h1>{{ element.original_title }}</h1>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: calc(100% / 3 - 10px);
+    /* debug */
+    background-color: blue;
+    height: 200px;
+
+}
+</style>
