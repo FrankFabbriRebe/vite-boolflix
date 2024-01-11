0e403d0f479324b52d79961a6c4934aebd1b98f7
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-11 11:33:23 +0100

    amend: content

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,6 +3,9 @@
 // import components
 import AppCard from './AppCard.vue';
 
+// import store
+import { store } from '../store';
+
 export default {
     name: "AppMain",
 
@@ -10,26 +13,38 @@ export default {
         AppCard
     },
 
+    data() {
+        return {
+            store,
+        }
+    },
+
 }
 
 </script>
 
 <template>
-    <div class="container">
-        <AppCard />
+    <div class="container" v-if="store.moviesArray.length !== 0">
+        <h1>Film</h1>
+        <div class="container_movie">
+            <AppCard />
+        </div>
     </div>
 </template>
 
 <style scoped>
 .container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 20px;
+}
+
+.container_movie {
     width: 80%;
     margin: 20px auto;
     display: flex;
     gap: 10px;
     flex-wrap: wrap;
-    /* justify-content: center; */
-    /* debug */
-    /* height: 500px;
-    border: 2px solid black; */
 }
 </style>
