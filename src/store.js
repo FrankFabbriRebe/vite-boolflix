97f039560ad55498b8418558846c9a86737ca310
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-11 12:20:17 +0100

    add: te series section

--- src/store.js
+++ src/store.js
@@ -2,8 +2,14 @@ import { reactive } from 'vue'
 
 export const store = reactive({
 
-    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=46ea676c2ff270a694d3966bc0f52218&language=it-IT',
+    // movies section
+    apiURLMovies: 'https://api.themoviedb.org/3/search/movie?api_key=46ea676c2ff270a694d3966bc0f52218&language=it-IT',
     moviesArray: [],
-    movieSearched: "",
+
+    // tv series section
+    apiUrlTvSeries: 'https://api.themoviedb.org/3/search/tv?api_key=46ea676c2ff270a694d3966bc0f52218&language=it-IT',
+    tvSeriesArray: [],
+
+    textSearched: "",
 
 })
