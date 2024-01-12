a50751f870f00c401b4cbf0e5ec7e610cbf92d77
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-12 10:47:30 +0100

    test: join movies and tv series container

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,6 +16,12 @@ export default {
     data() {
         return {
             store,
+
+            contentTypes: [
+                { list: 'moviesArray', title: 'Movies' },
+                { type: 'tvSeriesArray', list: 'tvSeriesArray', title: 'TV series' },
+            ],
+
         }
     },
 
@@ -24,21 +30,31 @@ export default {
 </script>
 
 <template>
-    <!-- movies container -->
-    <div class="container" v-if="store.moviesArray.length !== 0">
-        <h1>Movies</h1>
-        <div class="container_movie">
-            <AppCard v-for="(element, i) in store.moviesArray" :key="i" :info="element" />
+    <div>
+        <div v-for="(contentType, i) in contentTypes" :key="i" class="container"
+            v-if="store[contentType.list].length !== 0">
+            <h1>{{ contentType.title }}</h1>
+            <div class="container_movie">
+                <AppCard v-for="(media, i) in store[contentType.list]" :key="i" :info="media" :type="contentType.type" />
+            </div>
         </div>
     </div>
 
+    <!-- movies container -->
+    <!-- <div class="container" v-if="store.moviesArray.length !== 0">
+        <h1>Movies</h1>
+        <div class="container_movie">
+            <AppCard v-for="(element, i) in store.moviesArray" :key="i" :info="element" type="movies" />
+        </div>
+    </div> -->
+
     <!-- tv series container -->
-    <div class="container" v-if="store.tvSeriesArray.length !== 0">
+    <!-- <div class="container" v-if="store.tvSeriesArray.length !== 0">
         <h1>TV series</h1>
         <div class="container_movie">
-            <AppCard v-for="(element, i) in store.tvSeriesArray" :key="i" :info="element" />
+            <AppCard v-for="(element, i) in store.tvSeriesArray" :key="i" :info="element" type="tvSeries" />
         </div>
-    </div>
+    </div> -->
 </template>
 
 <style scoped>
